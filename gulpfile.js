225e8f450981a69d2cd3f7b2e7a57b7bb0fdc66e
Hunter Chang <hunter@madwiremedia.com>
2017-09-21 16:00:10 -0600

    disable hashed assets, for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,30 @@
 // http://danbahrami.io/articles/building-a-production-website-with-hugo-and-gulp-js/
 var gulp         = require("gulp"),
-    sass         = require("gulp-sass"),
-    hash         = require("gulp-hash"),
-    del          = require("del")
+    sass         = require("gulp-sass")
+    // hash         = require("gulp-hash"),
+    // del          = require("del")
 
 // Compile SCSS files to CSS
 gulp.task("scss", function () {
-    del(["static/css/**/*"])
+    // del(["static/css/**/*"])
     gulp.src("src/scss/**/*.scss")
         .pipe(sass({outputStyle : "compressed"}))
-        .pipe(hash())
+        // .pipe(hash())
         .pipe(gulp.dest("static/css"))
         //Create a hash map
-        .pipe(hash.manifest("hash.json"))
+        // .pipe(hash.manifest("hash.json"))
         //Put the map in the data directory
-        .pipe(gulp.dest("data/css"))
+        // .pipe(gulp.dest("data/css"))
 })
 
 // Hash javascript
 gulp.task("js", function () {
-    del(["static/js/**/*"])
+    // del(["static/js/**/*"])
     gulp.src("src/js/**/*")
-        .pipe(hash())
+        // .pipe(hash())
         .pipe(gulp.dest("static/js"))
-        .pipe(hash.manifest("hash.json"))
-        .pipe(gulp.dest("data/js"))
+        // .pipe(hash.manifest("hash.json"))
+        // .pipe(gulp.dest("data/js"))
 })
 
 // Watch asset folder for changes
