9313c870601bb8795807492ed1300461bb585c58
Hunter Chang <hunter@madwiremedia.com>
2017-09-22 11:19:18 -0600

    move gulp to theme folder

--- themes/bootstrap/LICENSE.md
+++ themes/bootstrap/LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2017 Murali Mohan Rath
+Copyright (c) 2017 Hunter Chang
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ themes/bootstrap/gulpfile.js
@@ -0,0 +1,40 @@
+// http://danbahrami.io/articles/building-a-production-website-with-hugo-and-gulp-js/
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass")
+    // hash         = require("gulp-hash"),
+    // del          = require("del")
+
+// Compile SCSS files to CSS
+gulp.task("scss", function () {
+    // del(["static/css/**/*"])
+    gulp.src([
+            "!src/scss/bootstrap/**/*", // Ignore the /bootstrap directory and all it's contents
+            "src/scss/**/*.scss"
+        ])
+        .pipe(sass({outputStyle : "compressed"}))
+        // .pipe(hash())
+        .pipe(gulp.dest("static/css"))
+        //Create a hash map
+        // .pipe(hash.manifest("hash.json"))
+        //Put the map in the data directory
+        // .pipe(gulp.dest("data/css"))
+})
+
+// Hash javascript
+gulp.task("js", function () {
+    // del(["static/js/**/*"])
+    gulp.src("src/js/**/*")
+        // .pipe(hash())
+        .pipe(gulp.dest("static/js"))
+        // .pipe(hash.manifest("hash.json"))
+        // .pipe(gulp.dest("data/js"))
+})
+
+// Watch asset folder for changes
+gulp.task("watch", ["scss", "js"], function () {
+    gulp.watch("src/scss/**/*", ["scss"])
+    gulp.watch("src/js/**/*", ["js"])
+})
+
+// Set watch as default task
+gulp.task("default", ["scss", "js"])
